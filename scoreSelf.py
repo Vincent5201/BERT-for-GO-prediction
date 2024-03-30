26693879e2cb637465ce3bd210b031af1b842137
Vincent <Vincent071659@gmail.com>
2024-03-30 15:51:22 +0800

    back to last ver

--- scoreSelf.py
+++ scoreSelf.py
@@ -3,10 +3,100 @@ from torch.utils.data import DataLoader
 import numpy as np
 from tqdm import tqdm
 
-from use import prediction
+from use import next_moves, prediction
 from myModels import get_model
-from myDatasets import get_datasets
+from myDatasets import transfer_back, channel_01, channel_2, transfer, channel_1015, get_datasets
 from dataAnalyze import plot_board
+
+def score_legal(data_type, num_moves, model, device):
+    first_steps = ["dd", "cd", "dc", "dp", "dq", "cp", "pd", "qd", 
+                   "pc", "pp", "pq", "qp"]
+    moves_score = 0
+    score = 0
+    full_score = len(first_steps)
+    records = []
+    for step in tqdm(first_steps, total = len(first_steps), leave = False):
+        games = [[step]]
+        datas = np.zeros([1,16,19,19],  dtype=np.float32)
+        step = transfer(step)
+        x = int(step / 19)
+        y = int(step % 19)
+        channel_01(datas, 0, x, y, len(games[0]))
+        channel_2(datas, 0)
+        while(len(games[0]) < num_moves):
+            next_move = next_moves(data_type, num_moves, model, games, 1, device)[0]
+            next_move_ch = transfer_back(next_move)
+            x = int(next_move / 19)
+            y = int(next_move % 19)
+            if datas[0][2][x][y]:
+                games[0].append(next_move_ch)
+                channel_01(datas, 0, x, y, len(games[0]))
+                channel_2(datas, 0)
+            else:
+                moves_score += len(games[0])
+                break
+        if len(games[0]) == num_moves:
+            score += 1
+            moves_score += num_moves
+        records.append(games[0])
+    return score, moves_score, full_score, records
+
+
+
+def score_feature(data_type, num_moves, model, bounds):
+    first_steps = ["dd", "cd", "dc", "dp", "dq", "cp", "pd", "qd", 
+                   "pc", "pp", "pq", "qp"]
+    nears = [0]*len(bounds)
+    total = 0
+    atari = 0
+    liberty = 0
+    records = []
+    for step in tqdm(first_steps, total = len(first_steps), leave = False):
+        games = [[step]]
+        datas = np.zeros([1,16,19,19],  dtype=np.float32)
+        f_step = transfer(step)
+        x = int(f_step / 19)
+        y = int(f_step % 19)
+        lastx = x
+        lasty = y
+        channel_01(datas, 0, x, y, len(games[0]))
+        channel_2(datas, 0)
+        liberty += channel_1015(datas, 0, x, y, len(games[0]))
+        while(len(games[0]) < num_moves):
+            next_move = next_moves(data_type, num_moves, model, games, 1)[0]
+            next_move_ch = transfer_back(next_move)
+            x = int(next_move / 19)
+            y = int(next_move % 19)
+            if datas[0][2][x][y]:
+                games[0].append(next_move_ch)
+                channel_01(datas, 0, x, y, len(games[0]))
+                channel_2(datas, 0)
+                liberty += channel_1015(datas, 0, x, y, len(games[0]))
+                total += 1
+                # distance
+                for i, bound in enumerate(bounds):
+                    if (pow(lastx-x, 2) + pow(lasty-y, 2)) < bound*bound:
+                        nears[i] += 1
+                lastx = x
+                lasty = y
+                # atari
+                p = 1
+                if len(games[0]) % 2:
+                    p = 0
+                if x > 0 and datas[0][p][x-1][y] and datas[0][10][x-1][y]:
+                    atari += 1
+                if y > 0 and datas[0][p][x][y-1] and datas[0][10][x][y-1]:
+                    atari += 1
+                if x < 18 and datas[0][p][x+1][y] and datas[0][10][x+1][y]:
+                    atari += 1
+                if y < 18 and datas[0][p][x][y+1] and datas[0][10][x][y+1]:
+                    atari += 1
+            else:
+                games[0].append(next_move_ch)
+                break
+        records.append(games[0])
+    return [near/total for near in nears], atari/total, liberty/total
+
 
 def myaccn_split(pred, true, n, split, num_move):
     correct = [0]*split
@@ -33,28 +123,6 @@ def correct_pos(pred, true):
             correct[i] /= total[i]
     return correct
 
-def detect_error(data_config, model, device):
-    _, testData = get_datasets(data_config, train=False)
-    test_loader = DataLoader(testData, batch_size=64, shuffle=False)
-    predl, true = prediction(data_config["data_type"], model, device, test_loader)
-    datas = testData.x.cpu().numpy()
-    total = len(predl)
-    error = 0
-    correct = 0
-    moves = 0
-    count_moves = 0
-    for i, p in tqdm(enumerate(predl), total=len(predl), leave=False):
-        sorted_indices = (-p).argsort()
-        x = int(sorted_indices[0]/19)
-        y = int(sorted_indices[0]%19)
-        if datas[i][2][x][y] == 0:
-            error += 1
-            moves += i%data_config["num_moves"]
-            count_moves += 1
-        if sorted_indices[0] == true[i]:
-            correct += 1
-    return error/total, moves/count_moves, correct/total
-
 def score_acc(data_config, model, split, device):
     batch_size = 64
     _, testData = get_datasets(data_config, train=False)
@@ -75,10 +143,19 @@ def correct_position(data_config, model, device):
 
 def score_self(data_config, model, score_type, device):
     
-    if score_type == "detect_error":
-        rate, acc = detect_error(data_config, model, device)
-        print(f'error:{rate}')
-        print(f'acc:{acc}')
+    if score_type == "score":
+        score, moves_score, full_score, records = score_legal(
+            data_config["data_type"], data_config["num_moves"], model, device)
+        #print(records)
+        print(f'score:{score}/{full_score}')
+        print(f'moves_score:{moves_score/full_score}/{data_config["num_moves"]}')
+    elif score_type == "feature":
+        bounds = [1.5, 2.9, 4.3, 5.7, 7.1, 8.5]
+        near, atari, liberty = score_feature(
+            data_config["data_type"], data_config["num_moves"], model, bounds)
+        print(f'near:{near}')
+        print(f'atari:{atari}')
+        print(f'liberty:{liberty}')
     elif score_type == "score_acc":
         #use test data
         split = 24
@@ -104,13 +181,13 @@ if __name__ == "__main__":
     data_config["num_moves"] = 240
 
     model_config = {}
-    model_config["model_name"] = "ResNet"
+    model_config["model_name"] = "ViT"
     model_config["model_size"] = "mid"
 
-    score_type = "detect_error"
+    score_type = "score"
     device = "cuda:0"
    
-    state = torch.load(f'models_{data_config["num_moves"]}/ResNet1_10000.pt')
+    state = torch.load(f'models_{data_config["num_moves"]}/ViT1_10000.pt')
     model = get_model(model_config).to(device)
     model.load_state_dict(state)
     
